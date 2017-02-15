43acb5fac59f34498affb4a2c52cc2ad1ccd9375
Petri Avikainen <petri.avikainen@gmail.com>
2017-02-15 21:40:20 +0200

    insert the url to db

--- web/controllers/song_controller.ex
+++ web/controllers/song_controller.ex
@@ -1,14 +1,25 @@
+require Logger
+
 defmodule Daily.SongController do
   use Daily.Web, :controller
-  require Logger
+
+  alias Daily.Song
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 
   def create(conn, %{"url" => url}) do
-    conn
-    |> put_flash(:info, "Successfully set the #{url} as daily song!")
-    |> render("index.html")
+    changeset = Song.changeset(%Song{}, %{url: url})
+    case Repo.insert(changeset) do
+      {:ok, _url} ->
+        conn
+        |> put_flash(:info, "Successfully set the #{url} as daily song!")
+        |> render("index.html")
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Could not set the #{url} as daily song!")
+        |> render("index.html", changeset: changeset)
+    end
   end
 end
