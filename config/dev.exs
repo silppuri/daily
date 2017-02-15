a9ddbe486265438c1f02c9f3f62669c5181dfcf9
Petri Avikainen <petri.avikainen@gmail.com>
2017-02-16 00:17:45 +0200

    js

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :daily, Daily.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch"]]
 
 
 # Watch static and templates for browser reloading.
---
+++ web/static/js/app.js
@@ -0,0 +1,8 @@
+// Import dependencies
+//
+// If you no longer want to use a dependency, remember
+// to also remove its path from "config.paths.watched".
+import socket from "./socket";
+
+console.log(socket);
+
