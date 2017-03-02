83341383d9b9b64093567cf51929ccbeec384c9a
Petri Avikainen <petri.avikainen@gmail.com>
2017-03-02 23:00:35 +0200

    update host

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :daily, Daily.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "boiling-lowlands-55915.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "boiling-lowlands-55915.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
