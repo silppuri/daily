ed71d036c153b13163dd344cb01fd273704241d5
Petri Avikainen <petri.avikainen@gmail.com>
2017-03-02 23:07:25 +0200

    play on mon-fri at 11:15

--- web/channels/daily_room_channel.ex
+++ web/channels/daily_room_channel.ex
@@ -9,12 +9,8 @@ defmodule Daily.DailyRoomChannel do
     {:ok, socket}
   end
 
-  def join("daily:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
-  end
-
   def handle_info({:pending}, socket) do
-    Quantum.add_job("15 11 * * *", fn ->
+    Quantum.add_job("15 11 * * 1-5", fn ->
       push socket, "play", %{videoId: Daily.Song.latest_video_id()}
     end)
     {:noreply, socket}
