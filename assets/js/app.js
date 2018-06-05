28b25b4035fb75120d83dafe25011748e3493c73
Petri Avikainen <petri.avikainen@gmail.com>
2018-06-05 21:55:23 +0300

    listen now button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,26 @@
 import socket from "./socket";
 
 let channel = socket.channel("daily:lobby", {});
+let playNowButton = document.querySelector("button");
+console.log(playNowButton);
 
-channel.join().receive("ok", resp => {
-  console.log("Joined");
-}).receive("error", resp => {
-  console.error("Unable to join", resp);
-});
+channel
+  .join()
+  .receive("ok", resp => {
+    console.log("Joined");
+  })
+  .receive("error", resp => {
+    console.error("Unable to join", resp);
+  });
 
-const loadVideo = player => ({ videoId }) => {
+const loadAndPlayVideo = player => ({ videoId }) => {
   player.loadVideoById(videoId);
   player.playVideo();
 };
 
 function onYouTubeIframeAPIReady() {
   let player = new YT.Player("player", { height: "390", width: "640" });
-  channel.on("play", loadVideo(player));
+  channel.on("play", loadAndPlayVideo(player));
 }
 
 window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-
